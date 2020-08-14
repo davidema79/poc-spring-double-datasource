d56786e66285571452357c16e60a116aec71c9bf
Davide Martorana <>
2020-08-15 00:33:50 +0100

    - Added Cloud Build config file - Added TestContainers dependency - Updated test using Testcontainers

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# poc-spring-double-datasource
+# Integration/Migration PoC: Spring with 2 DataSources
+
+## Build the project locally
+This project is configured to use Gradle 6.6 on jdk 11.
+
+Command:
+```shell
+  $ ./gradlew clean build -Ppg_user=<username> -Ppg_pwd=<password>
+``` 
+
+## Build the project on Cloud Build
+This project is ready to be built in GCP infrastructure using ***Cloud Build***. Deployment pipeline that executes the building steps and delivery of this project on Cloud Build are described in the file `cloudBuild.yaml` in the root directory.
+
+#### Editing Pipeline
+Edit the file `./cloudBuild.yaml` in order to modify the current pipeline.
+
+After editing that file one should test locally the changes before committing and push against the repository, using the utility `cloud-build-local`.
+In order to configure your local machine with **cloud-build-local**please follow the instructions in this [link](https://cloud.google.com/cloud-build/docs/build-debug-locally).
+
+Command:
+```shell script
+   $ cloud-build-local --config=cloudBuild.yaml --dryrun=false --push=false ./
+``` 
+
--- src/test/java/eu/davidemartorana/cloud/gcp/demo/DemoApplicationTests.java
+++ src/test/java/eu/davidemartorana/cloud/gcp/demo/DemoApplicationTests.java
@@ -1,10 +1,30 @@
 package eu.davidemartorana.cloud.gcp.demo;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class DemoApplicationTests {
+
+    @Container
+    private static final PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer("postgres:12")
+            .withDatabaseName("poc");
+
+    @DynamicPropertySource
+    public static void init(final DynamicPropertyRegistry propertyRegistry) {
+        propertyRegistry.add("app.datasources.first.url", postgresqlContainer::getJdbcUrl);
+        propertyRegistry.add("PG_USER", postgresqlContainer::getUsername);
+        propertyRegistry.add("PG_PWD", postgresqlContainer::getPassword);
+    }
+
 
     @Test
     void contextLoads() {
